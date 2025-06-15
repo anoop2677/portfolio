7a5aba88aa183e34c8df43ef12650dcee4581668
anoop2677 <kumaranoop527@gmail.com>
2025-06-15 07:17:46 +0200

    Updates

---
+++ static/js/916.d8916e5b.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 916.d8916e5b.chunk.js.LICENSE.txt */
+"use strict";(self.webpackChunkpersonal_site=self.webpackChunkpersonal_site||[]).push([[916],{2916:(c,e,l)=>{l.d(e,{y_8:()=>s});const s={prefix:"far",iconName:"envelope",icon:[512,512,[128386,9993,61443],"f0e0","M64 112c-8.8 0-16 7.2-16 16l0 22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1l0-22.1c0-8.8-7.2-16-16-16L64 112zM48 212.2L48 384c0 8.8 7.2 16 16 16l384 0c8.8 0 16-7.2 16-16l0-171.8L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64l384 0c35.3 0 64 28.7 64 64l0 256c0 35.3-28.7 64-64 64L64 448c-35.3 0-64-28.7-64-64L0 128z"]}}}]);
+//# sourceMappingURL=916.d8916e5b.chunk.js.map
